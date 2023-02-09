8f1ee6754b32de67cb8d270e54bf83a091c6a639
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-02-09 19:32:47 +0100

    trying to figure out redux

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,14 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-
+import Job from "./Job";
 
 const Favorites = () => {
-    let favorites = useSelector((state) => state.favorites.content)
+  let favorites = useSelector((state) => state.favorites.content);
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    return (
-        
-    )
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <ul></ul>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Favorites;
